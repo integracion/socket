7e9b936dafb1b154c8041ef69cd005cc4dfc8fc4
Neko <Neko@Neko.mshome.net>
2014-04-17 14:19:05 -0300

    holo :D

--- holanoe/src/holanoe/Holanoe.java
+++ holanoe/src/holanoe/Holanoe.java
@@ -17,6 +17,7 @@ public class Holanoe {
      */
     public static void main(String[] args) {
        System.out.println("hola neo push");
+       System.out.println("hola Seba :D");
     
     }
     
